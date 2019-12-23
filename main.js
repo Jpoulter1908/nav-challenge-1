cb4bf0d0761186915f714efeda5ebeda87888699
Jeremy Poulter <Jpoulter@gmail.com>
2019-12-22 20:28:51 -0700

    refactor Javascript to DRY it up

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 var allButtons = document.querySelectorAll('button');
 var globalListener = document.querySelector('body');
-var allTabs = document.querySelectorAll('.all-tabs');
-var allCoursesTab = document.querySelector('.all-courses-tab');
-var collectionsTab = document.querySelector('.collections-tab');
+var allTabs = document.querySelectorAll('.all-tabs-js');
+var allCoursesTab = document.querySelector('.all-courses-tab-js');
+var collectionsTab = document.querySelector('.collections-tab-js');
+var wishlistTab = document.querySelector('.wishlist-tab-js');
+var archivedTab = document.querySelector('.archived-tab-js');
 
 globalListener.addEventListener('click', clickHandler);
-globalListener.addEventListener('click', tabHandler);
 
 function clickHandler () {
-  if (event.target.classList.contains('button-option')) {
-    changeSelected();
+  if (event.target.classList.contains('ac-button-js')) {
+    selectTab(allCoursesTab);
+  }
+  else if (event.target.classList.contains('c-button-js')) {
+    selectTab(collectionsTab);
+  }
+  else if (event.target.classList.contains('w-button-js')) {
+    selectTab(wishlistTab);
+  }
+  else if (event.target.classList.contains('a-button-js')) {
+    selectTab(archivedTab);
   }
   else if (event.target.classList.contains('close-banner')) {
     closeBanner();
   }
+}
+
+function selectTab(chosenTab) {
+  changeSelected();
+  removeOtherTabs();
+  chosenTab.classList.remove('hidden');
 }
 
 function changeSelected () {
@@ -23,30 +39,13 @@ function changeSelected () {
   event.target.classList.add('selected');
 }
 
-function closeBanner() {
-  var parent = event.target.closest('.all-courses-tab');
-  parent.remove();
-}
-
-function tabHandler () {
-  if (event.target.classList.contains('ac-tab')) {
-    removeTheTabs();
-    allTabs.classList.remove('hidden');
-
-  }
-  else if (event.target.classList.contains('c-tab')) {
-
-  }
-  else if (event.target.classList.contains('w-tab')) {
-
-  }
-  else if (event.target.classList.contains('a-tab')) {
-
-  }
-}
-
-function removeTheTabs () {
+function removeOtherTabs () {
   for (var i = 0; i < allTabs.length; i++) {
     allTabs[i].classList.add('hidden');
   }
+}
+
+function closeBanner() {
+  var parent = event.target.closest('.all-courses-tab');
+  parent.remove();
 }
